d5bab9ec8edb929764648bcb358839be7f6eef39
nischalNikit <mailfornischal@gmail.com>
2020-01-29 18:57:32 +0530

    User Schema

---
+++ models/user.js
@@ -0,0 +1,16 @@
+//Packages
+var mongoose              = require("mongoose");
+var passportLocalMongoose = require("passport-local-mongoose");
+
+
+//Schema Setup 
+var UserSchema = new mongoose.Schema({
+      username: String,
+      password: String
+});
+UserSchema.plugin(passportLocalMongoose);
+
+
+//Export
+var User = mongoose.model("User",UserSchema);
+module.exports = User; 
