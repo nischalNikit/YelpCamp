92538591b38bb07ad52ac82d82971e9648278e4e
nischalNikit <mailfornischal@gmail.com>
2020-01-30 15:29:36 +0530

    Creating a User linking to the comment creation

--- models/comment.js
+++ models/comment.js
@@ -4,7 +4,13 @@ var mongoose = require("mongoose");
 //Comment Schema
 var CommentSchema = mongoose.Schema({
     text: String,
-    author: String
+    author: {
+        id: {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User"
+        }, 
+        username: String
+    }
 });
 var comment = mongoose.model("Comment",CommentSchema);
 
